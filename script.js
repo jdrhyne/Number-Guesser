dd2ff69d062f85f6365c2c1ef4179caa3282b3af
Jonathan Rhyne <jonathan@pspdfkit.com>
2021-01-04 00:21:50 -0500

    add script.js logic for game

--- script.js
+++ script.js
@@ -4,3 +4,32 @@ let currentRoundNumber = 1;
 
 // Write your code below:
 
+const generateTarget = () => {
+  return Math.floor(Math.random() * 10); // creates random number between 0 and 9
+};
+
+// Determines winner of guesses
+
+const compareGuesses = (humanGuess, computerGuess, targetGuess) => {
+    if (Math.abs(targetGuess - humanGuess) <= Math.abs(targetGuess - computerGuess)) {
+        return true; //if human difference to guess less than computer = human wins
+    } else {
+        return false; // otherwise computer wins
+    }
+}
+
+// Updates Score in the Game
+
+const updateScore = (winner) => {
+    if (winner === 'human') {
+        humanScore++;
+    } else if (winner === 'computer') {
+        computerScore++; 
+    }
+}
+
+// Increase Round by 1
+
+const advanceRound = () => {
+    currentRoundNumber++;
+};
